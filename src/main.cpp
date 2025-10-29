54edfbd17f35eac5ed30a9d09407d2e3b4261948
Dirk Arnez <smalldirkalex@gmail.com>
2025-10-29 20:26:30 +0800

    - add src/main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include <RWGltf_CafReader.hxx>
+#include <RWGltf_CafWriter.hxx>
+// Meshing
+#include <BRepMesh_IncrementalMesh.hxx>
+#include <TopoDS_Shape.hxx>
+#include <RWMesh_CoordinateSystem.hxx>
